14b85ed0142bf08221888a5f03993a556378126e
Bartłomiej Budzik <bartlomiej.budzik@gmail.com>
2018-05-30 20:02:15 +0200

    Add Passed or Failed

--- passed_or_failed.rb
+++ passed_or_failed.rb
@@ -1,3 +1,23 @@
 class PassedOrFailed
+  attr_accessor :args
+  def initialize(*args)
+    self.args = *args
+  end
 
+  def perform
+    threshold = args[1].to_i
+    hash = args[0]
+    pass = {}
+    fails = {}
+    hash.each do |key, value|
+      if value.to_i >= threshold
+        pass[key] = value.to_i
+      else
+        fails[key] = value.to_i
+      end
+    end
+    expected_hash = { :passed => pass, :failed => fails}
+  end
+
+  private
 end
